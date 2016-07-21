65fe0ea5d9157bfc164d280ee8eab54a0fa8c695
matheesh-k <kmatheesh396@gmail.com>
2016-07-21 18:28:59 +0530

    Create prefix_longest.java

---
+++ prefix_longest.java
@@ -0,0 +1,21 @@
+public class prefix_longest { 
+	public static void main(String a[]) {
+		int i_val = 10, j_val = 20;
+		boolean chk;
+		chk = i_val < j_val;
+		System.out.println("chk value: "+chk);
+		int source[] = {1, 2, 3, 4, 5, 6};
+		int dest[] = new int[10];
+		System.arraycopy(source,5, dest,6,2);
+		System.out.println(dest[7]);
+		
+			int x[ ][ ]=new int[10] [ ];
+			System.out.println(x[4][0]);
+			
+		
+			int[] b = new int[]{1,2,3};
+			for(int i : b)
+			System.out.println(i);
+			}
+			
+		}
